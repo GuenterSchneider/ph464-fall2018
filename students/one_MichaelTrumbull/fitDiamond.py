6f0d50864a36647aaed5fc993aa9aaa8f1a606d0
Trumbu <trumbumi@oregonstate.edu>
2018-10-01 15:37:37 -0700

    looking for minimum

--- students/one_MichaelTrumbull/fitDiamond.py
+++ students/one_MichaelTrumbull/fitDiamond.py
@@ -8,7 +8,7 @@ then finds two polyfits x in [3,10]
 
 ### Load data ###
 print('hello world')
-data = np.loadtxt('data/data.txt')
+data = np.loadtxt('data.txt')
 print (data)
 
 x = data[:,0]
@@ -18,17 +18,21 @@ y = data[:,1]
 plt.plot(x,y,"o",label="data")
 
 ### Fit data ###
-x_fit = np.linspace(3,10,100)
-### n=3 ###
+x_fit = np.linspace(3,10,500)
+### parabolic ###
 z = np.polyfit(x,y,3)
 fit = np.poly1d(z)
 y_fit = fit(x_fit)
-plt.plot(x_fit,y_fit,label="n=3 fit")
-### n=4 ###
+plt.plot(x_fit,y_fit,"-r",label="n=3 fit")
+min = min(y_fit)
+
+print('Minimum located at: ', min)
+
+### n=4 fit ###
 z = np.polyfit(x,y,4)
 fit = np.poly1d(z)
 y_fit = fit(x_fit)
-plt.plot(x_fit,y_fit,label="n=4 fit")
+plt.plot(x_fit,y_fit,"-b",label="n=4 fit")
 
 plt.legend()
 plt.xlabel('x')
